061724688441dcec72a039a7a4c6da5ba64f2eae
Ken Erickson <bookworm.productions@gmail.com>
2011-10-22 08:09:51 -0700

    configure padrino-sprockets

--- app/app.rb
+++ app/app.rb
@@ -5,5 +5,18 @@ class DbdDocs < Padrino::Application
   register CompassInitializer   
   register Padrino::Sprockets   
   
-  enable :sessions
+  enable :sessions 
+  
+  assets do    
+    digest false  
+    handle_stylesheets false  
+    stylesheets_url 'css'
+    javascripts_url 'js'  
+    public_assets_folder '/'
+    public_stylesheets_folder 'css'  
+    public_javascripts_folder 'js'
+    append_path 'assets/js'     
+    append_path '../lib/assets/js'
+    append_path '../vendor/assets/js' 
+  end
 end
